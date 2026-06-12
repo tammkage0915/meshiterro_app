4e22b3effb85a97baf9e646618458ae7d3f10ef2
tammkage0915 <280695020+tammkage0915@users.noreply.github.com>
2026-06-12 11:15:44 +0000

    [c]post_2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
   get 'homes/about' => 'homes#about', as: 'about'
   resources :users, only: [:new, :create] , path_names: { new: 'sign_up' }
 
