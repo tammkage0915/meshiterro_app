40c9dc96a38ec904ad6314500f69f7f0d26dbd3e
tammkage0915 <280695020+tammkage0915@users.noreply.github.com>
2026-06-15 07:12:36 +0000

    [finish]sns_2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   get 'homes/about' => 'homes#about', as: 'about'
